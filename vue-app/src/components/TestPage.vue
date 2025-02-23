51107ea6c334e56f2cab1c47d18e6379f8dcdee3
SvetlanaShipovalova <90263099+SvetlanaShipovalova@users.noreply.github.com>
2025-02-23 12:10:49 +0300

    add 3 tests and change structure of files

---
+++ vue-app/src/components/TestPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="currentComponent" />
+</template>
+
+<script setup>
+import {defineAsyncComponent, computed, defineProps} from 'vue';
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  }
+});
+
+const testComponents = {
+  '1': defineAsyncComponent(() => import('../views/Test1.vue')),
+  '2': defineAsyncComponent(() => import('../views/Test2.vue')),
+  '3': defineAsyncComponent(() => import('../views/Test3.vue')),
+  //'4': defineAsyncComponent(() => import('../views/Test4.vue')),
+  //'5': defineAsyncComponent(() => import('../views/Test5.vue')),
+};
+
+const currentComponent = computed(() => {
+  return testComponents[props.id] || null;
+});
+</script>
